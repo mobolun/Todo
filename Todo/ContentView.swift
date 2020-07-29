737dc2299a0e36649f9a99c1af41dbc25bf0ef76
mobolun <mobolun@icloud.com>
2020-07-30 06:18:04 +0800

    Initial Commit

---
+++ Todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Todo
+//
+//  Created by 二爷 on 2020/7/30.
+//  Copyright © 2020 二爷. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
