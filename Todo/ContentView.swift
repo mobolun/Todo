c0dba16d8e4d6865cd012d175a24f8fd6997015c
mobolun <mobolun@icloud.com>
2020-08-07 06:12:19 +0800

    test

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -9,10 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+//            Rectangle()
+//                .foregroundColor(Color.gray)
+////                .frame(width:100%)
+            Text("抓住 zack")
+                .font(.callout)
+                .padding(.leading)
+            Spacer()
+            Image(systemName: "bolt.circle.fill")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(width: 35,height: 35)
+                .imageScale(.large)
+                .foregroundColor(Color.red)
+                .padding(.trailing)
+            
+        }
+        .frame(height: 40)
+        .background(Color.white)
+        .cornerRadius(30)
+        .shadow(radius: 4)
+        .padding(.horizontal)
     }
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
