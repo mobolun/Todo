926d149bb242554a85e6aa7bdadee231c8e69f2d
mobolun <mobolun@icloud.com>
2020-08-14 13:27:24 +0800

    数据传递,并在外部文件中生成一个指定格式的数组

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -10,37 +10,112 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @ObservedObject var userDate:ToDo = ToDo(data: [
+    singleToDo(title: "买一个奔"),
+    singleToDo(title: "买一个驰"),
+    singleToDo(title: "买一个慢")
+    ])
+    
+    
+    var body: some View {
+        ScrollView (.vertical, showsIndicators: true) {
+            VStack (spacing: 20) {
+                
+                ForEach(self.userDate.toDoList) {
+                    Item(title: $0.title)
+                }
+                
+                
+//                ForEach(0..<20){
+//                    Item(title: String($0))
+//                }
+                
+            }
+            .padding(.vertical)
+        }
+        
+    }
+}
+
+struct Item: View {
+@State private var isChecked:Bool = true
+@State var title:String = ""
+@State var date = Date()
+    var body: some View {
+     HStack () {
+         RoundedRectangle(cornerRadius:  20)
+             .foregroundColor(.gray)
+             .frame(width: UIScreen.main.bounds.size.width * 0.7)
+         Spacer()
+         
+     }
+     .frame(height: 40)
+     .background(Color.white)
+     .cornerRadius(20)
+     .shadow(radius: 5)
+     .padding(.horizontal)
+     .overlay(
+             HStack {
+                VStack (alignment: .leading, spacing: 4) {
+                    Text(self.title)
+                         .font(.callout)
+                        .padding(.leading)
+                    Text(self.date.description)
+                        .foregroundColor(Color.blue)
+                        .font(.footnote)
+                        .padding(.leading)
+                }
+                 Spacer()
+                 Image(systemName:self.isChecked ? "bolt.circle": "bolt.circle.fill")
+                     .resizable()
+                     .aspectRatio(contentMode: .fit)
+                     .frame(width: 28)
+                     .foregroundColor(Color.red)
+                     .padding(.trailing)
+                     .onTapGesture {
+                         self.isChecked.toggle()
+                        
+                 }
+             }
+             .padding(.horizontal)
+         )
+    }
+}
+
+struct title: View {
     var body: some View {
         HStack {
-//            Rectangle()
-//                .foregroundColor(Color.gray)
-////                .frame(width:100%)
-            Text("抓住 zack")
-                .font(.callout)
-                .padding(.leading)
-            Spacer()
-            Image(systemName: "bolt.circle.fill")
-//                .resizable()
-//                .scaledToFit()
-//                .frame(width: 35,height: 35)
-                .imageScale(.large)
-                .foregroundColor(Color.red)
-                .padding(.trailing)
             
+            
+            
+            Image(systemName: "tag")
+                .resizable()
+                .frame(width: 25, height: 25)
+                .foregroundColor(Color.red)
+//                .padding()
+                
+            Spacer()
+            Text("已完成")
+            Spacer()
+            
+            Button(action: {print("tag")}){
+                
+                Image(systemName: "ellipsis")
+                   .resizable()
+                   .scaledToFill()
+                   .foregroundColor(Color.red)
+                   .frame(width: 5, height: 5)
+//                   .padding()
+            }
         }
-        .frame(height: 40)
-        .background(Color.white)
-        .cornerRadius(30)
-        .shadow(radius: 4)
         .padding(.horizontal)
     }
 }
 
-
-
-
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+#endif
---
+++ Todo/UserData.swift
@@ -0,0 +1,42 @@
+//
+//  UserData.swift
+//  Todo
+//
+//  Created by 二爷 on 2020/8/11.
+//  Copyright © 2020 二爷. All rights reserved.
+//
+
+
+import Foundation
+
+
+//创建一个类,用来生成一个列表,名字不可与项目名相同
+class ToDo:ObservableObject {
+    @Published var toDoList:[singleToDo] = []
+    var count:Int = 0
+    
+    init(data:[singleToDo]) {
+        for i in data {
+            self.toDoList.append(singleToDo(title: i.title, id: self.count))
+            count += 1
+        }
+    }
+    
+    //向外部提供一个可以切换开关
+    func check(id:Int) {
+        self.toDoList[id].isChecked.toggle()
+    }
+    
+    
+}
+
+
+
+
+//创建一个结构体,用来规定数据的结构
+struct singleToDo:Identifiable {
+    var title:String = ""
+    var isChecked:Bool = true
+    var date:Date = Date()
+    var id:Int = 0
+}
